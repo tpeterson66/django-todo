b0f47d062e5ddbd8c595b77cd35fec327355dd29
Tom Peterson <tom.peterson@coretek.com>
2020-10-04 22:32:30 -0400

    fixing issue with docker-compose file

--- todoApp/todoApp/settings.py
+++ todoApp/todoApp/settings.py
@@ -81,10 +81,8 @@ DATABASES = {
         'NAME': os.environ['POSTGRES_NAME'],
         'USER': os.environ['POSTGRES_USER'],
         'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-        # 'HOST': os.environ['POSTGRES_HOST'],
-        'HOST': '172.18.0.3',
-        # 'PORT': os.environ['POSTGRES_PORT']
-        'PORT': 5432
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT']
     }
 }
 
