2d8c5b6479eecc7c31cb4d4f0df529eaff8aa9a4
Tom Peterson <tom.peterson@coretek.com>
2020-11-16 09:10:03 -0500

    adding info for Azure App Services and PostgresDB

---
+++ terraform/vars.tf
@@ -0,0 +1,10 @@
+variable "client_id" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_secret" {}
+variable "db_username" {
+    default = "postgres"
+}
+variable "db_password" {
+    default = "thisSuperSECUREPassword2020!"
+}
